b418cac4f02320b5db5ef0241d05ebd9e7ea4330
yurii_fishbakh <bunbanto@gmail.com>
2025-05-29 15:55:58 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-// import './js/faq.js';
-// import './js/my-projects.js';
 import './js/features.js';
-import './js/burger-menu.js';
 import './js/faq.js';
-// import './js/switcher.js';
-// import './js/work-together.js';
 import './js/scrollup.js';
